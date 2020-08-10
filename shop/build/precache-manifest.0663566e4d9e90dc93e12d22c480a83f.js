8f1afd93905cc0def1bc4d13c90e8d9ffe784036
Peter Shevchuk <raf02041994@gmail.com>
2020-08-10 18:20:21 +0300

    building

---
+++ shop/build/precache-manifest.0663566e4d9e90dc93e12d22c480a83f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a97c75472d6381d080cd65171428e38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15466ec62b7608c07e36",
+    "url": "/static/css/main.1ec8dda3.chunk.css"
+  },
+  {
+    "revision": "7267bda7d782e1dc55a5",
+    "url": "/static/js/2.9b9c0295.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b9c0295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15466ec62b7608c07e36",
+    "url": "/static/js/main.29d247f8.chunk.js"
+  },
+  {
+    "revision": "705c69162250a325d3b6",
+    "url": "/static/js/runtime-main.f2e4f4fd.js"
+  }
+]);
