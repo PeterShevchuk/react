2b54d4f5c1b07057c2565beb0fabc43775e93f40
Peter Shevchuk <raf02041994@gmail.com>
2020-08-11 17:27:57 +0300

    Updates

---
+++ precache-manifest.2c188619511e10d10146abb13b5a14cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af53846463891524d12ae826d313fd0f",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "2ceb24fad8720fe091dd",
+    "url": "/react/static/css/main.cf3d6129.chunk.css"
+  },
+  {
+    "revision": "c2faede906b99e86be35",
+    "url": "/react/static/js/2.79238a02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.79238a02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ceb24fad8720fe091dd",
+    "url": "/react/static/js/main.c20f1e07.chunk.js"
+  },
+  {
+    "revision": "f3dec3074284f66b272f",
+    "url": "/react/static/js/runtime-main.597948fa.js"
+  }
+]);
